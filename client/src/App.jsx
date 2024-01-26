3c75cce12e4623866be8c404ad091cee47dedb47
Dhanushvardan <avj25012003@gmail.com>
2024-01-26 22:02:14 +0530

    completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,23 @@
 import React, { useEffect, useRef, useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import Peer from "simple-peer";
 import io from "socket.io-client";
 import "./App.scss";
 
 const socket = io.connect("http://localhost:5000");
 function App() {
+  const [me, setMe] = useState("");
+  const [stream, setStream] = useState();
+  const [receivingCall, setReceivingCall] = useState(false);
+  const [caller, setCaller] = useState("");
+  const [callerSignal, setCallerSignal] = useState();
+  const [callAccepted, setCallAccepted] = useState(false);
+  const [idToCall, setIdToCall] = useState("");
+  const [callEnded, setCallEnded] = useState(false);
+  const [name, setName] = useState("");
   const myVideo = useRef();
   const userVideo = useRef();
   const connectionRef = useRef();
-  const [stream, setStream] = useState();
-  const [myId, setMyId] = useState();
-  const [receivingCall, setReceivingCall] = useState(false);
-  const [name, setName] = useState();
-  const [from, setFrom] = useState();
-  const [callerSignal, setCallerSignal] = useState();
-  const [callAccepted, setCallAccepted] = useState(false);
-  const [me, setMe] = useState();
-  const [caller, setCaller] = useState();
-  const [idToCall, setIdToCall] = useState();
 
   useEffect(() => {
     navigator.mediaDevices
@@ -26,9 +26,11 @@ function App() {
         setStream(stream);
         myVideo.current.srcObject = stream;
       });
+
     socket.on("me", (id) => {
-      setMyId(id);
+      setMe(id);
     });
+
     socket.on("callUser", (data) => {
       setReceivingCall(true);
       setCaller(data.from);
@@ -43,7 +45,6 @@ function App() {
       trickle: false,
       stream: stream,
     });
-
     peer.on("signal", (data) => {
       socket.emit("callUser", {
         userToCall: id,
@@ -52,11 +53,9 @@ function App() {
         name: name,
       });
     });
-
     peer.on("stream", (stream) => {
       userVideo.current.srcObject = stream;
     });
-
     socket.on("callAccepted", (signal) => {
       setCallAccepted(true);
       peer.signal(signal);
@@ -64,25 +63,110 @@ function App() {
 
     connectionRef.current = peer;
   };
+
+  const answerCall = () => {
+    setCallAccepted(true);
+    const peer = new Peer({
+      initiator: false,
+      trickle: false,
+      stream: stream,
+    });
+    peer.on("signal", (data) => {
+      socket.emit("answerCall", { signal: data, to: caller });
+    });
+    peer.on("stream", (stream) => {
+      userVideo.current.srcObject = stream;
+    });
+
+    peer.signal(callerSignal);
+    connectionRef.current = peer;
+  };
+
+  const leaveCall = () => {
+    setCallEnded(true);
+    connectionRef.current.destroy();
+  };
+
   return (
-    <div className="App">
-      <h1>hello</h1>
-      {stream && (
-        <video
-          playsInline
-          muted
-          ref={myVideo}
-          autoPlay
-          style={{ width: "40vw", height: "40vh" }}
-        />
-      )}
-      <input
-        placeholder="Enter id to call"
-        onChange={(e) => {
-          setIdToCall(e.target.value);
-        }}
-      ></input>
-    </div>
+    <>
+      <h1 style={{ textAlign: "center", color: "#fff" }}>
+        Video call Interface for Capstone using webRTC
+      </h1>
+      <div className="container">
+        <div className="video-container">
+          <div className="video">
+            {stream && (
+              <video
+                playsInline
+                muted
+                ref={myVideo}
+                autoPlay
+                style={{ width: "300px" }}
+              />
+            )}
+          </div>
+          <div className="video">
+            {callAccepted && !callEnded ? (
+              <video
+                playsInline
+                ref={userVideo}
+                autoPlay
+                style={{ width: "300px" }}
+              />
+            ) : null}
+          </div>
+        </div>
+        <div className="myId">
+          <input
+            id="filled-basic"
+            label="Name"
+            variant="filled"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            style={{ marginBottom: "20px" }}
+          />
+          <CopyToClipboard text={me} style={{ marginBottom: "2rem" }}>
+            <button variant="contained" color="primary">
+              Copy ID
+            </button>
+          </CopyToClipboard>
+
+          <input
+            id="filled-basic"
+            label="ID to call"
+            variant="filled"
+            value={idToCall}
+            onChange={(e) => setIdToCall(e.target.value)}
+          />
+          <div className="call-button">
+            {callAccepted && !callEnded ? (
+              <button variant="contained" color="secondary" onClick={leaveCall}>
+                End Call
+              </button>
+            ) : (
+              <button
+                color="primary"
+                aria-label="call"
+                onClick={() => callUser(idToCall)}
+              >
+                Call
+              </button>
+            )}
+            {idToCall}
+          </div>
+        </div>
+        <div>
+          {receivingCall && !callAccepted ? (
+            <div className="caller">
+              <h1>{name} is calling...</h1>
+              <button variant="contained" color="primary" onClick={answerCall}>
+                Answer
+              </button>
+            </div>
+          ) : null}
+        </div>
+      </div>
+    </>
   );
 }
 
