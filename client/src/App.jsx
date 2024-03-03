390f67825ef6af7929ec3acb6d016f8a25145cc7
Dhanushvardan <avj25012003@gmail.com>
2024-03-03 09:09:24 +0530

    backend_testing_Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,8 @@ import io from "socket.io-client";
 import "./App.scss";
 
 // const socket = io.connect("https://socket-backend-jlbk.onrender.com");
-const socket = io.connect("https://backend-cap-utts.onrender.com");
+//const socket = io.connect("https://backend-cap-utts.onrender.com");
+const socket = io.connect("https://backend-testing-g6ch.onrender.com");
 function App() {
   const [me, setMe] = useState("");
   const [stream, setStream] = useState();
